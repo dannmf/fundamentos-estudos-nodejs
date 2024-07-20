12c8d46fd5d84ec82fa3d4419d0b9aa45768159b
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-20 15:48:52 -0300

    💡 entendendo streams writable, readable, transform

--- src/streams/streams.js
+++ src/streams/streams.js
@@ -8,12 +8,13 @@
 // Utilizando Streams esse processo é feito de forma mais eficiente
 // O arquivo é lido em pedaços e enviado para o banco de dados
 
-// Readable Streams: são streams que podem ser lidas
-// Writable Streams: são streams que podem ser escritas
-
+// Readable Streams: são streams que podem ser lidas (Ler Dados)
+// Writable Streams: são streams que podem ser escritas (Escrever Dados)
+// Transform Streams: são streams que podem transformar os dados (Transformar Dados) é utilizada no intermediário entre a stream de leitura e a strean de escrita
+// Duplex Streams: são streams que podem ser lidas e escritas (Ler e Escrever Dados)
 // process.stdin.pipe(process.stdout);  lê o que é digitado no terminal e escreve no terminal e o pipe faz a conexão entre os dois
 
-import { Readable } from 'node:stream';
+import { Readable, Writable, Transform, Duplex } from 'node:stream';
 
 class OneToHundredStream extends Readable {
     index = 1
@@ -30,4 +31,20 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout) // imprime de 1 a 100 no terminal
+class InverseNumber extends Transform {
+    _transform(chunk, encoding, callback){
+        const transformed = Number(chunk.toString()) * -1
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback){
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
+new OneToHundredStream()
+.pipe(new InverseNumber())
+.pipe(new MultiplyByTenStream()) // imprime de 1 a 100 no terminal
