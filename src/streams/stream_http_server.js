84df4c0eb5d6c9df02b2040d61b76ac8c82e1c07
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-21 14:23:09 -0300

    💡 entendendo streams com http

---
+++ src/streams/stream_http_server.js
@@ -0,0 +1,24 @@
+import http from 'node:http'
+import { Transform } from 'node:stream'
+
+class InverseNumber extends Transform {
+    _transform(chunk, encoding, callback){
+        const transformed = Number(chunk.toString()) * -1
+
+        console.log(transformed)
+
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+// Todas portas de entrada e saida no node são Streams
+// req => Readable Stream
+// res => Writable Stream
+
+const server = http.createServer((req, res) => {
+    return req
+    .pipe(new InverseNumber())
+    .pipe(res)
+})
+
+server.listen(3334)
