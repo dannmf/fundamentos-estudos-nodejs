257ac094f20c0a8da63a982ebcc805e36b50b0d6
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-22 19:36:41 -0300

    💡 entendendo buffer e criando middleware transformar a req em json via stream

---
+++ src/streams/buffer.js
@@ -0,0 +1,7 @@
+// Buffer:
+// O buffer é um espaço de memória alocado para armazenar dados temporários
+// E tem grande performance na transição de dados
+
+const buf = Buffer.from("Hingridy")
+
+console.log(buf)
