36a14ef200147e2640dc0c0bf138e720acc08264
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-22 22:42:40 -0300

    💡 criação de regex para pegar o parametro no route params

--- src/routes.js
+++ src/routes.js
@@ -30,7 +30,18 @@ export const routes = [
 
             return res.writeHead(201).end()
         }
+    },
+
+    {
+        method: 'DELETE',
+        path: '/users/:id',
+        handler: (req, res) => {
+
+        }
+
     }
+
+
 
 
 ]
