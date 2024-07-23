edd620e2627fee6b74f2a95d31e698695f6c2667
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-22 21:26:21 -0300

    💡 implementação de um "banco de dados" via json

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
-
+import { Database } from './database.js';
+import { randomUUID } from 'node:crypto';
 // Métodos HTTP
 // GET: Buscar informações do back-end
 // POST: Criar uma informação no back-end
@@ -33,7 +34,7 @@ import { json } from './middlewares/json.js';
 // 5xx: Server Error
 //   500: Internal Server Error
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
     const { url, method } = req;
@@ -42,18 +43,20 @@ const server = http.createServer(async (req, res) => {
 
 
     if (method === 'GET' && url === '/users') {
+        const users = database.select('users')
         return res
-           
             .end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users'){
         const { name, email } = req.body
-        users.push({
-            id: 1,
+        const user = ({
+            id: randomUUID(),
             name: name,
             email: email,
         })
+
+        database.insert('users', user)
 
         return res.writeHead(201).end()
     }
